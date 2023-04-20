881769bc0c2b15f017c23fadc4e10edfc7254bf9
bhiwde <nirmlarameshbhiwde@gmail.com>
2023-04-20 16:20:46 +0530

    [ADDED]: [Nirmla] Check The Age For Driving Licence

---
+++ Drive.java
@@ -0,0 +1,13 @@
+class Drive
+{
+public static void main(String args[])
+{
+  int no;
+  no = Integer.parseInt( args[0] );
+   
+  if(no>=18){
+System.out.println("You Have Allowed Driving License");}
+else{
+System.out.println("You Have Not Allowed Driving License");}
+ }
+}
